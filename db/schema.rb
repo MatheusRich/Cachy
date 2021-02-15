9614a66525b374dc35d32f5965fa67c5ea1d25b2
Matheus Richard <matheusrichardt@gmail.com>
2021-02-15 17:37:03 -0300

    Add books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_201151) do
+ActiveRecord::Schema.define(version: 2021_02_15_202018) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -18,4 +18,13 @@ ActiveRecord::Schema.define(version: 2021_02_15_201151) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times { Author.find_or_create_by(name: Faker::Name.unique.name) }
+50.times { Book.find_or_create_by(title: Faker::Book.title, author_id: Author.ids.sample) }
